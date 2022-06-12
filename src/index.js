59b619459e9353429b7e3452d1f5e80c72fb72ef
Lavenderleaff <oceansidesummers@proton.me>
2022-06-12 12:14:31 -0700

    Command Handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+
+const fs = reqire('fs');
+
 require('dotenv').config();
 
-client.on('ready', () => {
-    console.log('Signed in!');
-});
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith("js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith("js"));
+const commandFolders = fs.readdirSync("./src/commands");
 
 (async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    
     client.login(process.env.TOKEN);
 })();
